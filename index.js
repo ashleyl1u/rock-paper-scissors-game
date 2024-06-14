78501593cc0b87ccc2c486d9df580179ce9246da
Ashley Liu <ashleyl1u095@gmail.com>
2024-06-14 15:01:33 -0400

    Create getPlayerMove() function

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('HELLo');
+let playerMove;
+
+playerMove = getPlayerMove();
+console.log(playerMove);
+
+function getPlayerMove () {
+  let move = prompt ('Enter your move (Rock, Paper, Scissors): ');
+  move = move.toLowerCase();
+  
+  while ( move !== 'rock' && move !== 'paper' && move !== 'scissors'){
+    move = prompt ('Invalid! Renter your move (Rock, Paper, Scissors): ');
+    move = move.toLowerCase();
+  }
+  
+  return move;
+}
+
+
